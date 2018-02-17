aee0c4cd05cf6eba10b3047c91c81167a0e7336c
Andrew Ammon <andrewammon@Andrews-MBP.home>
2018-02-17 12:17:01 -0500

    server switch to 8080

--- jacks-app/index.js
+++ jacks-app/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 const mustacheExpress = require("mustache-express");
 
