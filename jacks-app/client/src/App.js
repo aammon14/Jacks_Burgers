359a970f9fab49ff6324e59edcbfa5b7f009162b
Keith <Keith@servd.com>
2018-02-21 09:57:23 -0500

    sign up working

--- jacks-app/client/src/App.js
+++ jacks-app/client/src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
     super(props);
 
     this.state = {
-      users: {},
+      user: null,
       items: [],
       cart: [],
       orders: [],
@@ -31,6 +31,7 @@ class App extends Component {
     this.getAllOrders = this.getAllOrders.bind(this);
     this.changeOrderState = this.changeOrderState.bind(this);
     this.changeCartState = this.changeCartState.bind(this);
+    this.changeUserState = this.changeUserState.bind(this);
     this.getCart = this.getCart.bind(this);
   }
 
@@ -117,6 +118,10 @@ class App extends Component {
 
   changeCartState(cart) {
     this.setState({ cart: cart });
+  }
+
+  changeUserState(user) {
+    this.setState({ user: user });
   }
 
   render() {
@@ -239,8 +244,9 @@ class App extends Component {
                 return (
                   <Signup
                     {...props}
-                    users={this.state.users}
+                    state={this.state}
                     getAllUser={this.getAllUser}
+                    changeUserState={this.changeUserState}
                   />
                 );
               }}
@@ -253,6 +259,7 @@ class App extends Component {
                     {...props}
                     users={this.state.users}
                     getAllUser={this.getAllUser}
+                    changeUserState={this.changeUserState}
                   />
                 );
               }}
