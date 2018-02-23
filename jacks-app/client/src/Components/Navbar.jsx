9ad8a8bae7f4a05f6e205ae70e3cecce87bf009c
Andrew Ammon <andrewammon@ool-ad0201e3.dyn.optonline.net>
2018-02-23 10:45:47 -0500

    nav

--- jacks-app/client/src/Components/Navbar.jsx
+++ jacks-app/client/src/Components/Navbar.jsx
@@ -2,18 +2,31 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 class Navbar extends Component {
-
-	render(){
-return(
-<div className='nav_bar'>
-
-  <div className="nav_links"><Link to='/Sign_in'>Login</Link></div>
-  <div className="nav_links"><Link to='/Sign_up'>Sign up</Link></div>
-  <div className="nav_links"><Link to='/orders'>Previous Orders</Link></div>
-	 <div className="nav_links"><Link to='/kitchen'>Kitchen Orders</Link></div>
-  <div className="nav_links"><Link to="/Menu_Maintenance">Admin</Link></div>
-  <div className="nav_links"><Link to="/users/profile">Profile</Link></div>
-
-</div>
-)}}
+   render() {
+       return (
+           <div className="nav_bar">
+               <div className="nav_links">
+                   <Link className="nav_none" to="/orders">
+                       Previous Orders
+                   </Link>
+               </div>
+               <div className="nav_links">
+                   <Link className="nav_none" to="/kitchen">
+                       Kitchen Orders
+                   </Link>
+               </div>
+               <div className="nav_links">
+                   <Link className="nav_none" to="/Menu_Maintenance">
+                       Menu Maintenance
+                   </Link>
+               </div>
+               <div className="nav_links right_link">
+                   <Link className="nav_none" to="/users/profile">
+                       Profile
+                   </Link>
+               </div>
+           </div>
+       );
+   }
+}
 export default Navbar;
