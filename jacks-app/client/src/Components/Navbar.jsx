c0bbaff6d8f5ba6639a4d1302d14b7d9fc2f4553
Andrew Ammon <andrewammon@ool-ad0201e3.dyn.optonline.net>
2018-02-23 10:40:57 -0500

    playing with css dont merge

--- jacks-app/client/src/Components/Navbar.jsx
+++ jacks-app/client/src/Components/Navbar.jsx
@@ -2,18 +2,16 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 class Navbar extends Component {
+	render() {
+    return(
+      <div className='nav_bar'>
+        <div className="nav_links"><Link to='/orders'>Past Orders</Link></div>
+	      <div className="nav_links"><Link to='/kitchen'>Order Queue</Link></div>
+        <div className="nav_links"><Link to="/Menu_Maintenance">Admin</Link></div>
+        <div className="nav_links"><Link to="/users/profile">Profile</Link></div>
+      </div>
+    )
+  }
+}
 
-	render(){
-return(
-<div className='nav_bar'>
-
-  <div className="nav_links"><Link to='/Sign_in'>Login</Link></div>
-  <div className="nav_links"><Link to='/Sign_up'>Sign up</Link></div>
-  <div className="nav_links"><Link to='/orders'>Previous Orders</Link></div>
-	 <div className="nav_links"><Link to='/kitchen'>Kitchen Orders</Link></div>
-  <div className="nav_links"><Link to="/Menu_Maintenance">Admin</Link></div>
-  <div className="nav_links"><Link to="/users/profile">Profile</Link></div>
-
-</div>
-)}}
 export default Navbar;
